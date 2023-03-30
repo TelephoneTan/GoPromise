548940092ed13be5864186314e8c997a42d09491
telephone <telephone.tan@qq.com>
2023-03-30 19:09:32 +0800

    OnceTask 新增接口
    【OnceTask 新增接口】
    * Cancel 取消任务
    * DoJob 执行任务以完成指定的工作

--- async/task/once.go
+++ async/task/once.go
@@ -12,22 +12,36 @@ type Once[T any] struct {
 	promise   *promise.Type[T]
 }
 
+func NewOnceTask[T any](job promise.Job[T]) *Once[T] {
+	return (&Once[T]{Job: job}).Init()
+}
+
+func NewOnceTaskWithSemaphore[T any](job promise.Job[T], semaphore *promise.Semaphore) *Once[T] {
+	return (&Once[T]{Job: job, Semaphore: semaphore}).Init()
+}
+
+func NewOnceTaskEmpty[T any]() *Once[T] {
+	return (&Once[T]{}).Init()
+}
+
 func (o *Once[T]) Init() *Once[T] {
 	return o
 }
 
+func (o *Once[T]) Cancel() {
+	o.once.Do(func() {
+		o.promise = promise.Cancelled[T]()
+	})
+	o.promise.Cancel()
+}
+
+func (o *Once[T]) DoJob(job promise.Job[T]) *promise.Type[T] {
+	o.once.Do(func() {
+		o.promise = promise.NewPromiseWithSemaphore(job, o.Semaphore)
+	})
+	return o.promise
+}
+
 func (o *Once[T]) Do() *promise.Type[T] {
-	return (&promise.Type[T]{
-		Job: promise.Job[T]{
-			Do: func(rs promise.Resolver[T], re promise.Rejector) {
-				o.once.Do(func() {
-					o.promise = (&promise.Type[T]{
-						Job:       o.Job,
-						Semaphore: o.Semaphore,
-					}).Init()
-				})
-				rs.ResolvePromise(o.promise)
-			},
-		},
-	}).Init()
+	return o.DoJob(o.Job)
 }
